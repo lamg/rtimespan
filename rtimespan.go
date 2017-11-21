5873c59098f3630ff7a58ba98e5648a44c055152
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2017-11-21 16:09:37 -0500

    Initial

---
+++ rtimespan.go
@@ -0,0 +1,46 @@
+// Package rtimespan determines whether a time is
+// contained in a time span that repeates several
+// times (possibly infinite).
+//
+// As an example you may consider a time span that
+// starts at determined time for an hour, and you
+// also want to specify an homologal time spans that
+// start a day later, for 10 days (includes current day).
+// For representing that situtation you would create an
+// RSpan with start time determined by you, active duration
+// of one hour, total duration of 24 hours and Times
+// field equal 10. Notice that the 10th day counting
+// from the start date is excluded.
+package rtimespan
+
+import (
+	"time"
+)
+
+// RSpan is a repeated time span
+type RSpan struct {
+	// Time the repeated span starts
+	Start time.Time
+	// Duration of the active phase
+	Active time.Duration
+	// Duration of the cycle
+	Total time.Duration
+	// Number of times the span repeats
+	Times int
+	// Repeats forever
+	Infinite bool
+	// AllTime indicates whether all times are
+	// contained in this span, i.e. for all times
+	// RSpan.ContainsTime returns true
+	AllTime bool
+}
+
+// ContainsTime returns whether r contains x
+func (r *RSpan) ContainsTime(x time.Time) (y bool) {
+	p := x.Sub(r.Start)
+	d, m := p/r.Total, p%r.Total
+	y = r.AllTime ||
+		(((r.Times >= 0 && d < time.Duration(r.Times)) ||
+			r.Infinite) && m < r.Active)
+	return
+}
---
+++ rtimespan_test.go
@@ -0,0 +1,101 @@
+package rtimespan
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRSpan(t *testing.T) {
+	// ts[0] is the date the RSpan starts
+	// ts[1:] are the dates to be tested whether
+	// they are inside the RSpan with an hour of
+	// active period, a day of duration and repeated 10 times.
+
+	// The boolean values in the following comments indicate
+	// whether the associated date belongs the mentioned
+	// repeated time span.
+	ts := []string{
+		// RSpan start date - 0
+		"2006-01-02T15:04:05-04:00",
+		// One minute later - 1 true
+		"2006-01-02T15:05:05-04:00",
+		// 59 minutes with 59 seconds later - 2 true
+		"2006-01-02T16:04:04-04:00",
+		// 1 hour later - 3 false
+		"2006-01-02T16:04:05-04:00",
+		// One day later - 4 true
+		"2006-01-03T15:04:05-04:00",
+		// One day later minus one minute - 5 false
+		"2006-01-03T15:04:04-04:00",
+		// 9 days later plus one minute - 6 true
+		"2006-01-11T15:05:05-04:00",
+		// 10 days later plus one minute - 7 false
+		"2006-01-12T15:05:05-04:00",
+		// 11 days later plus one minute - 8 false
+		"2006-01-13T15:05:05-04:00",
+	}
+	tp := make([]time.Time, len(ts))
+	for i, j := range ts {
+		var e error
+		tp[i], e = time.Parse(time.RFC3339, j)
+		require.NoError(t, e)
+	}
+	rs := &RSpan{
+		Active:   time.Hour,
+		Infinite: false,
+		Start:    tp[0],
+		Times:    10,
+		Total:    24 * time.Hour,
+		AllTime:  false,
+	}
+	tss := []struct {
+		rs *RSpan
+		t  time.Time
+		y  bool
+	}{
+		{rs, tp[1], true},
+		{rs, tp[2], true},
+		{rs, tp[3], false},
+		{rs, tp[4], true},
+		{rs, tp[5], false},
+		{rs, tp[6], true},
+		{rs, tp[7], false},
+		{rs, tp[8], false},
+	}
+	for i, j := range tss {
+		a := j.rs.ContainsTime(j.t)
+		require.True(t, a == j.y, "At %d %t != %t", i, a, j.y)
+	}
+}
+
+func Example() {
+	t0, e := time.Parse(time.RFC3339,
+		"2006-01-02T15:04:05-04:00")
+	var t1 time.Time
+	if e == nil {
+		// a day later inside the active time span
+		// which is 2006-01-03T15:04:05-04:00 ->
+		// 2006-01-03T16:04:05-04:00
+		t1, e = time.Parse(time.RFC3339,
+			"2006-01-03T15:46:05-04:00")
+	}
+	if e == nil {
+		rs := &RSpan{
+			Active:   time.Hour,
+			Infinite: false,
+			Start:    t0,
+			Times:    10,
+			Total:    24 * time.Hour,
+		}
+		// A repeated time span of on hour each day,
+		// during 10 days, starting on t0
+		y := rs.ContainsTime(t1)
+		fmt.Println(y)
+		// Output: true
+	} else {
+		println(e.Error())
+	}
+}
